a20dc0933f2ceb9e58b561b20f3d4a91f5117615
Christopher Evan <christopher.evan0603@gmail.com>
2023-03-13 21:00:16 +0700

    init

---
+++ app/src/main/java/com/yeet/adv160420011week2/GameFragment.kt
@@ -0,0 +1,43 @@
+package com.yeet.adv160420011week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (arguments != null) {
+            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
+            val txtTurn = view.findViewById<TextView>(R.id.txtTurn)
+            txtTurn.text = "$playerName's Turn"
+        }
+
+        val btnBack = view.findViewById<Button>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
